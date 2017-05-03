046266dfb997dc5b530ee0a514dac98ed36f35bd
Daniel Quinlan <DaniRaiQ@gmail.com>
2017-05-03 01:24:56 +0100

    Add files via upload

---
+++ models/user-store.js
@@ -0,0 +1,31 @@
+/**
+ * Created by dquinlan on 02/05/17.
+ */
+'use strict';
+
+const = require('lodash');
+const JsonStore = require('./json-store');
+
+const userStore = {
+
+    store:new JsonStore('./models/user-store.json', {users: []}),
+    collection: 'users',
+
+    getAllUsers() {
+        return this.store.findAll)this.collection);
+    },
+
+    addUser(user) {
+        this.store.add(this.collection, user);
+    },
+
+    getUserById(id) {
+        return this.store.findOneBy(this.collection, {id: id});
+    },
+
+    getUserByEmail(email) {
+        return this.store.findOneBy(this.collection, {email: email});
+    },
+}
+
+module.exports = userStore;
